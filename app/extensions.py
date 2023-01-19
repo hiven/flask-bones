7faba38de5ed274dabe0d2b9247e3da4dc94d9d0
hiven <mail@hiven.com>
2023-01-19 21:21:26 +0000

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,4 @@
 from flask_assets import Environment
-from flask_babel import Babel
 from flask_bcrypt import Bcrypt
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -11,7 +10,6 @@ from flask_travis import Travis
 from werkzeug.contrib.cache import SimpleCache
 
 assets = Environment()
-babel = Babel()
 bcrypt = Bcrypt()
 cache = SimpleCache()
 limiter = Limiter(key_func=get_remote_address)
