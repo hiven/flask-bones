d250f83c537fd87b84f3b5e5505103deaaa3a45a
hiven <mail@hiven.com>
2023-01-19 20:52:50 +0000

    Update forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,4 @@
 from flask_wtf import Form
-from flask_babel import gettext
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired
 
@@ -7,8 +6,8 @@ from app.user.models import User
 
 
 class LoginForm(Form):
-    username = TextField(gettext('Username'), validators=[DataRequired()])
-    password = PasswordField(gettext('Password'), validators=[DataRequired()])
+    username = TextField(('Username'), validators=[DataRequired()])
+    password = PasswordField(('Password'), validators=[DataRequired()])
 
     def __init__(self, *args, **kwargs):
         Form.__init__(self, *args, **kwargs)
@@ -22,15 +21,15 @@ class LoginForm(Form):
         self.user = User.query.filter_by(username=self.username.data).first()
 
         if not self.user:
-            self.username.errors.append(gettext('Unknown username'))
+            self.username.errors.append(('Unknown username'))
             return False
 
         if not self.user.check_password(self.password.data):
-            self.password.errors.append(gettext('Invalid password'))
+            self.password.errors.append(('Invalid password'))
             return False
 
         if not self.user.active:
-            self.username.errors.append(gettext('User not activated'))
+            self.username.errors.append(('User not activated'))
             return False
 
         return True
