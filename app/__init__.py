98c38a6cee3d53506fc863444e7e2c30591b8a52
hiven <mail@hiven.com>
2023-01-19 21:20:29 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,6 @@ from app.commands import create_db, drop_db, populate_db, recreate_db
 from app.database import db
 from app.extensions import lm, travis, mail, migrate, bcrypt, babel, rq, limiter
 from app.user import user
-from app.utils import url_for_other_page
-
 
 def create_app(config=config.base_config):
     """Returns an initialized Flask application."""
@@ -53,7 +51,6 @@ def register_extensions(app):
     lm.init_app(app)
     mail.init_app(app)
     bcrypt.init_app(app)
-    babel.init_app(app)
     rq.init_app(app)
     migrate.init_app(app, db)
     limiter.init_app(app)
