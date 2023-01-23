1dc15de2e708f755ccafac9ef256c57fdaec58c5
hiven <mail@hiven.com>
2023-01-23 19:55:30 +0000

    Update jobs.py

--- app/jobs.py
+++ app/jobs.py
@@ -1,35 +1,22 @@
 from flask import render_template
-#from flask_mail import Message
-from flask_mailing import Mail, Message
-
+from flask_mail import Message
 
 from app.extensions import mail, rq
 from app.user.models import User
 
 
 #@rq.job
-#def send_registration_email(uid, token):
-#    """Sends a registratiion email to the given uid."""
-#    user = User.query.filter_by(id=uid).first()
-#    msg = Message(
-#        'User Registration',
-#        sender='admin@flask-bones.com',
-#        recipients=[user.email]
-#    )
-#    msg.body = render_template(
-#        'mail/registration.mail',
-#        user=user,
-#        token=token
-#    )
-#    mail.send(msg)
-
-
-async def send_registration_email(uid, token):
-    message = Message(
-        subject="Flask-Mailing module",
-        recipients=["test@yahoo.com"],
-        body="This is the basic email body",
-        )
-
-    await mail.send_message(message)
-    return jsonify(status_code=200, content={"message": "email has been sent"})
+def send_registration_email(uid, token):
+    """Sends a registratiion email to the given uid."""
+    user = User.query.filter_by(id=uid).first()
+    msg = Message(
+        'User Registration',
+        sender='admin@flask-bones.com',
+        recipients=[user.email]
+    )
+    msg.body = render_template(
+        'mail/registration.mail',
+        user=user,
+        token=token
+    )
+    mail.send(msg)
