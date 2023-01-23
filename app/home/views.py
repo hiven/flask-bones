d700ef30e0b29f8f052bc1be1a0736bc8020b6cc
hiven <mail@hiven.com>
2023-01-23 12:30:14 +0000

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -4,3 +4,15 @@ from ..home import home
 @home.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@home.route('/terms', methods=['GET'])
+def terms():
+    return render_template('terms.html')
+
+@home.route('/privacy', methods=['GET'])
+def privacy():
+    return render_template('privacy.html')
+
+@home.route('/security', methods=['GET'])
+def security():
+    return render_template('security.html')
