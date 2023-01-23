e3d15ef89a23baea1349b56fe567c5d86c75b62b
hiven <mail@hiven.com>
2023-01-23 10:54:57 +0000

    Update views.py

--- app/home/views.py
+++ app/home/views.py
@@ -1,14 +1,6 @@
-from flask import current_app, flash, render_template, request, redirect, url_for
-from app import db
-from . import main_blueprint
+from flask import Flask, g, render_template, request
 
 
-
-#@main_blueprint.route('/', methods=['GET', 'POST'])
-#def home():
-#    return render_template('main/index.html')
-  
-  
 @home_bp.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
