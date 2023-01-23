fb4123decea674e74d884ed7230cd7888d27eff5
hiven <mail@hiven.com>
2023-01-23 20:00:20 +0000

    Update extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -3,8 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_login import LoginManager
-#from flask_mail import Mail
-from flask_mailing import Mail
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_rq2 import RQ
 from flask_travis import Travis
